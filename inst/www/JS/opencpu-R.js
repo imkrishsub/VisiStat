df537ac1dca3197916ce51c931cb260d4e859f94
krishna221 <krishna.prasath.god@gmail.com>
2013-11-29 08:57:21 +0100

    ts

--- inst/www/JS/opencpu-R.js
+++ inst/www/JS/opencpu-R.js
@@ -13,7 +13,8 @@ function loadFile(filePath)
         
     //for each variable, get the data and the IQR
     for(var i=0; i<output.variableNames.length; i++)
-    {    
+    {      
+        console.dir(dataset);
         for(var j=0; j<dataset[variableNames[i]].length; j++)
         {
             if(dataset[i][j] == "")
