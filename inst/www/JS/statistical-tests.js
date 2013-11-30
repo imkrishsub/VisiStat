6959fa9bb166a76267192ab41e3cb1be96ad8203
krishna221 <krishna.prasath.god@gmail.com>
2013-11-30 15:30:16 +0100

    ts

--- inst/www/JS/statistical-tests.js
+++ inst/www/JS/statistical-tests.js
@@ -106,31 +106,33 @@ function loadAssumptionCheckList()
                 .attr("x", 0)
                 .attr("y", i*assumptionStep + assumptionOffsetTop - assumptionImageSize/2 - 10)
                 .attr("text-anchor", "end")
-                .attr("xlink:href", "images/tick.png")
-                .attr("height", assumptionImageSize)            
-                .attr("width", assumptionImageSize)
-                .attr("display", "none")
-                .attr("id", assumptions[i])
-                .attr("class", "ticks");
-            canvas.append("image")
-                .attr("x", 0)
-                .attr("y", i*assumptionStep + assumptionOffsetTop - assumptionImageSize/2)
-                .attr("text-anchor", "end")
                 .attr("xlink:href", "images/checkingAssumptions.gif")
                 .attr("height", assumptionImageSize)            
                 .attr("width", assumptionImageSize)
                 .attr("id", assumptions[i])
-                .attr("class", "loading");                
-            canvas.append("image")
-                .attr("x", 0)
-                .attr("y", i*assumptionStep + assumptionOffsetTop - assumptionImageSize/2)
-                .attr("text-anchor", "end")
-                .attr("xlink:href", "images/cross.png")
-                .attr("height", assumptionImageSize)
-                .attr("width", assumptionImageSize)
-                .attr("display", "none")
-                .attr("id", assumptions[i])
-                .attr("class", "crosses");
+                .attr("class", "loading");
+                
+//             canvas.append("image")
+//                 .attr("x", 0)
+//                 .attr("y", i*assumptionStep + assumptionOffsetTop - assumptionImageSize/2 - 10)
+//                 .attr("text-anchor", "end")
+//                 .attr("xlink:href", "images/tick.png")
+//                 .attr("height", assumptionImageSize)            
+//                 .attr("width", assumptionImageSize)
+//                 .attr("display", "none")
+//                 .attr("id", assumptions[i])
+//                 .attr("class", "ticks");
+//                          
+//             canvas.append("image")
+//                 .attr("x", 0)
+//                 .attr("y", i*assumptionStep + assumptionOffsetTop - assumptionImageSize/2)
+//                 .attr("text-anchor", "end")
+//                 .attr("xlink:href", "images/cross.png")
+//                 .attr("height", assumptionImageSize)
+//                 .attr("width", assumptionImageSize)
+//                 .attr("display", "none")
+//                 .attr("id", assumptions[i])
+//                 .attr("class", "crosses");
         }    
     }, 1300);
     
