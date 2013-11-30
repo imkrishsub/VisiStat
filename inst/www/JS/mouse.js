85c399fe3b3831eba2e1854764c0f6543df686f2
krishna221 <krishna.prasath.god@gmail.com>
2013-11-30 14:36:06 +0100

    ts

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -991,8 +991,8 @@ function OnMouseOver(e)
                 .attr("class", "hover");    
         
         canvas.append("text")
-                .attr("x", mouseX + scaleForWindowSize(10))
-                .attr("y", mouseY + scaleForWindowSize(15))
+                .attr("x", mouseX + scaleForWindowSize(15))
+                .attr("y", mouseY + scaleForWindowSize(25))
                 .attr("text-anchor", "middle")
                 .text(format(getActualValue(outlier.attr("cy"))))
                 .attr("class", "hover");
