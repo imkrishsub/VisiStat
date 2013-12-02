b3bb9f60fcd12f20c8eb81c4f0c1884101037fbf
krishna221 <krishna.prasath.god@gmail.com>
2013-12-02 14:15:10 +0100

    problem with median computation

--- inst/www/JS/math.js
+++ inst/www/JS/math.js
@@ -19,14 +19,15 @@ function mean(values)
 
 function median(values) 
 {
-    values.sort( function(a,b) {return a - b;} );
+    temp = values;
+    temp.sort( function(a,b) {return a - b;} );
 
-    var half = Math.floor(values.length/2);
+    var half = Math.floor(temp.length/2);
 
-    if(values.length % 2)
-        return values[half];
+    if(temp.length % 2)
+        return temp[half];
     else
-        return (values[half-1] + values[half]) / 2.0;
+        return (temp[half-1] + temp[half]) / 2.0;
 }
 
 function findNumberOfCombinations(n, y)
