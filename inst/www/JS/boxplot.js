c633d6b119b28f70bd71b4f1d6134305aa8ba4be
krishna221 <krishna.prasath.god@gmail.com>
2013-12-02 15:15:38 +0100

    damn bugs

--- inst/www/JS/boxplot.js
+++ inst/www/JS/boxplot.js
@@ -383,7 +383,7 @@ function makeBoxplot()
                         .attr("id", ids[i])
                         .attr("class", "means")
                         .attr("data-indepenentVariableA", dataAttributeForIndependentVariableA)
-                        .attr("data-indepenentVariableB", dataAttributeForIndependentVariableB))
+                        .attr("data-indepenentVariableB", dataAttributeForIndependentVariableB)
                         .attr("data-levelA", temp[0])
                         .attr("data-levelB", temp[1]);
         }        
