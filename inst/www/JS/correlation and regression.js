4d3599394159c1cc267a65f80fb78d87182bdced
krishna221 <krishna.prasath.god@gmail.com>
2013-12-10 20:16:51 +0100

    ws

--- inst/www/JS/correlation and regression.js
+++ inst/www/JS/correlation and regression.js
@@ -29,7 +29,7 @@ function getCorrelationCoefficient(variableA, variableB, method)
                     
                     logResult();
                     
-                    if(allVariablesAreNumeric())
+//                     if(allVariablesAreNumeric())
                         drawButtonInSideBar("CONSTRUCT MODEL", "regression");
                 }
                 else if(method == "kendall")
@@ -50,7 +50,7 @@ function getCorrelationCoefficient(variableA, variableB, method)
                     
                     logResult();
                     
-                    if(allVariablesAreNumeric())
+//                     if(allVariablesAreNumeric())
                         drawButtonInSideBar("CONSTRUCT MODEL", "regression");
                 }
                 
