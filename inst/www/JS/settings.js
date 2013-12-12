2229250b20222d56f7d4bef9d9fc6dca18b890d4
krishna221 <krishna.prasath.god@gmail.com>
2013-12-12 17:08:57 +0100

    s

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -100,7 +100,7 @@ var meanColors = new Object(); //Colors for mean, and ?
     meanColors["click"] = "#729e38";
 
 // Histogram
-var nBins = 7; 
+var nBins = 5; 
 var binCountFontSize = "16px";
 var histLegendOffsetX = scaleForWindowSize(45);
 var histLegendOffsetY = scaleForWindowSize(45);
