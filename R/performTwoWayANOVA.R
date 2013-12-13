08c05ee06d3c3aab58bc02ec6ee7e8b8f8bc5d29
krishna221 <krishna.prasath.god@gmail.com>
2013-12-13 15:36:12 +0100

    tos

--- R/performTwoWayANOVA.R
+++ R/performTwoWayANOVA.R
@@ -7,7 +7,7 @@ performTwoWayANOVA <- function(dataset, dependentVariable, participantVariable, 
     
     result = eval(parse(text = paste("ezANOVA(table, dv = ", dependentVariable,", wid = ", participantVariable, ", between = c(", betweenGroupVariableA, ",", betweenGroupVariableB, "))", sep="")));
     
-    result = result["ANOVA"];
+    result = result$ANOVA;
     
-    list(numDF = result["DFn"], denomDF = result["DFd"], F = result["F"], p = result["p"], etaSquared = result["ges"]);
+    list(numDF = result$DFn, denomDF = result$DFd, F = result$F, p = result$p, etaSquared = result$ges);
 }
