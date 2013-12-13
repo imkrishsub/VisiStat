c5d91a20ddd107510ebe7157bb4baf199e074a9a
krishna221 <krishna.prasath.god@gmail.com>
2013-12-13 23:39:45 +0100

    st

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -130,7 +130,7 @@ var labelOffset = scaleForWindowSize(45);
 // Significance test
 var significanceTestScaleOffset = scaleForWindowSize(25);
 var assumptionStep = scaleForWindowSize(55);
-var assumptionOffsetTop = assumptionStep*3;
+var assumptionOffsetTop = assumptionStep*1.5;
 var assumptionImageSize = scaleForWindowSize(35);
 
 var assumptionsText = new Object();
