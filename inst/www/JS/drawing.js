ff761250baa576727b2bd2708a0f5208e474bc8f
krishna221 <krishna.prasath.god@gmail.com>
2013-12-17 14:12:09 +0100

    fix

--- inst/www/JS/drawing.js
+++ inst/www/JS/drawing.js
@@ -420,7 +420,7 @@ function drawEffectSize(value)
                                     .attr("class", "effectSize");
     }
     
-    if(abs(scale(min + (value - 0))) > effectSizeWidth/4)
+    if(Math.abs(scale(min + (value - 0))) > effectSizeWidth/4)
     {    
         sideBar.append("text")
                 .attr("x", L + scale(0) + scale(min + (value - 0)) - yAxisTickTextOffset)
