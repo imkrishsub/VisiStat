74b1b89c92fec5a9d65357d40443dd305600441d
krishna221 <krishna.prasath.god@gmail.com>
2013-12-17 15:11:53 +0100

    e

--- inst/www/JS/scatterplot matrix.js
+++ inst/www/JS/scatterplot matrix.js
@@ -393,7 +393,7 @@ function makeScatterPlotAt(x,y,shortWidth, shortHeight, variableX, variableY, no
 
 function convertToRange(number, min, max)
 {
-    if(isNaN(number-min)
+    if(isNaN(number-min))
     {
         console.log(number);
         console.log(min);
