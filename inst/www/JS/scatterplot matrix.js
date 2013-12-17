bba4537d270fcdd7933b257548665e22e85306e8
krishna221 <krishna.prasath.god@gmail.com>
2013-12-17 14:59:04 +0100

    debugging

--- inst/www/JS/scatterplot matrix.js
+++ inst/www/JS/scatterplot matrix.js
@@ -234,6 +234,9 @@ function makeScatterPlotAt(x,y,shortWidth, shortHeight, variableX, variableY, no
     
         x2 = x + getValue(X2, minX, maxX)*shortWidth;
         y2 = y - getValue(Y2, minY, maxY)*shortHeight;            
+        
+        console.log("x1=" + x1 + ", x2=" + x2);
+        console.log("y1=" + y1 + ", y2=" + y2);
     
         canvas.append("line")
                 .attr("x1", x1)
