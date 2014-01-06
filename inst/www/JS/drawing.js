949f1d3bfe23c2df22bfe4a57fd475d066edb5fd
krishna221 <krishna.prasath.god@gmail.com>
2014-01-06 12:46:03 +0100

    x

--- inst/www/JS/drawing.js
+++ inst/www/JS/drawing.js
@@ -847,8 +847,7 @@ function drawScales(cx, cy)
     if(1)
     {
         testResults["CI"] = [8.36, 13.36];
-        
-        console.log(format.log(means[means.length - 1] - means[0]);
+    
         if(testResults["CI"][1] - dec2(means[means.length -1] - means[0]) != dec2(means[means.length -1] - means[0]) - testResults["CI"][0])   
         {
             testResults["CI"][0] = -testResults["CI"][0];
