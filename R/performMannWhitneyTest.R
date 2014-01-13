f9e52d7203882d9e84798e329dcbc805bfa2aef7
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-13 16:09:36 +0100

    added ez package back

--- R/performMannWhitneyTest.R
+++ R/performMannWhitneyTest.R
@@ -12,15 +12,7 @@ performMannWhitneyTest <- function(groupA, groupB)
     p = result$p.value;
     CI = result$conf.int;
  
-    
-    g <- factor(c(rep("groupA", length(groupA)), rep("groupB", length(groupB))));
-    v <- c(groupA, groupB);
-    
-    result <- wilcox_test(v~g, distribution = "exact");
-    
-    Z = result@statistic@teststatistic[["groupA"]];
-    
-    r = Z/length(groupA);
+    Z = 
     
     list(U = U, p = p, r = abs(r), error = error);
 }
