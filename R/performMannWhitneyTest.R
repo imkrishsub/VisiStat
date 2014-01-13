b504553d9c87277e7d767334b12954f01c47cb98
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-13 18:40:52 +0100

    fixed a bug with mann-whitney test

--- R/performMannWhitneyTest.R
+++ R/performMannWhitneyTest.R
@@ -11,6 +11,10 @@ performMannWhitneyTest <- function(groupA, groupB)
     U = result$statistic[["W"]];
     p = result$p.value;
     CI = result$conf.int;
+
+    g <- factor(c(rep("GroupA", length(groupA)), rep("GroupB", length(groupB))))
+    v <- c(groupA, groupB)
+    wilcox_test(v ~ g, distribution="exact")
  
     result = coin::wilcox_test(groupA ~ groupB, distribution = "exact");    
     name <- names(result@statistic@teststatistic);
