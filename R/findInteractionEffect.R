17e78128840dfc40ccb76f8e85e84c7c22b3b8a1
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-13 19:42:27 +0100

    no message

--- R/findInteractionEffect.R
+++ R/findInteractionEffect.R
@@ -26,7 +26,7 @@ findInteractionEffect <- function(dependentVariable, independentVariables, datas
     
     if(length(independentVariables) == 2)
     {
-        result <- eval(parse(text = paste("effect(term=\"",independentVariables[1],":",independentVariables[2],"\", model)",sep="")));
+        result <- eval(parse(text = paste("effects::effect(term=\"",independentVariables[1],":",independentVariables[2],"\", model)",sep="")));
     }
     
     list(fit = result$fit);
