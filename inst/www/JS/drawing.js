247ceabe46a6aaca22108752d3f8b9be7e793c0e
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-14 16:31:00 +0100

    tooltip border for variable type toggle button

--- inst/www/JS/drawing.js
+++ inst/www/JS/drawing.js
@@ -1665,8 +1665,18 @@ function displayToolTips()
                                     .attr("stroke", "#3957F1")
                                     .attr("stroke-dasharray","3,3")
                                     .attr("class","toolTips");
-    // var variableTypeSelectionBorder = canvas.append("rect")
-    //                                         .attr("x", variableNameHolderWidth - variableTypeSelectionButtonWidth + 2*variableNameHolderPadding - variableNameHolderPadding/2)
+                                    
+    var variableTypeSelectionBorder = canvas.append("rect")
+                                            .attr("x", variableNameHolderWidth - variableTypeSelectionButtonWidth + 2*variableNameHolderPadding - variableNameHolderPadding/2)
+                                            .attr("y", variableNameHolderPadding + i*(variableNameHolderHeight + variableNameHolderPadding) + scaleForWindowSize(2) - variableNameHolderPadding/2)
+                                            .attr("height", variableNameHolderHeight)
+                                            .attr("width", (variableNameHolderHeight - variableNameHolderPadding/2)/1.75 + variableNameHolderPadding)
+                                            .attr("rx", "5px")
+                                            .attr("ry", "5px")
+                                            .attr("fill","none")
+                                            .attr("stroke", "#3957F1")
+                                            .attr("stroke-dasharray","3,3")
+                                            .attr("class","toolTips");
 }
 
     
