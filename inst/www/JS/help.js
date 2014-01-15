e6a5a99de40c3ea449695ded9ca3f877c028d279
krishna <krishna.prasath.subramanian@rwth-aachen.de>
2014-01-15 14:46:12 +0100

    totally revamped help

--- inst/www/JS/help.js
+++ inst/www/JS/help.js
@@ -33,9 +33,9 @@ desc["method"]["fT"] = "Friedman's test is used when 3 or more conditions are co
 desc["method"]["WA"] = "Welch's ANOVA is used when 3 or more conditions are compared and the independent variable is a between-groups factor (different participants are used for different conditions). It is an alternative to the one-way ANOVA. It can be used used when homogeneity of variances is not met (i.e., the distributions have unequal variances). It still requires the distributions to follow normal distributions.";
 desc["method"]["kwT"] = "Kruskal-Wallis test is used when 3 or more conditions are compared and the independent variable is a between-groups factor (different participants are used for different conditions). It is an alternative to the one-way ANOVA. It can be used used when distributions are not normal. However, it requires homogeneity of variances to be met (i.e., the distributions must have equal variances)."; 
 
-desc["method"]["pC"] = "Pearson's correlation coefficient is used to find correlation (the change in one variable as the other variable changes) between two ratio variables. It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
-desc["method"]["kC"] = "Kendall's correlation coefficient is used to find correlation (the change in one variable as the other variable changes) when an interval variable is present. It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
-desc["method"]["bC"] = "Biserial correlation coefficient is used to find correlation (the change in one variable as the other variable changes) when one of the variables is binary (can take only two values). It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
+desc["method"]["pC"] = "Pearson's correlation coefficient is used to find correlation (the direction of change in one variable as the other variable changes) between two ratio variables. It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
+desc["method"]["kC"] = "Kendall's correlation coefficient is used to find correlation (the direction of change in one variable as the other variable changes) when an interval variable is present. It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
+desc["method"]["bC"] = "Biserial correlation coefficient is used to find correlation (the direction of change in one variable as the other variable changes) when one of the variables is binary (can take only two values). It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
 
 desc["method"]["ptT"] = "Post-hoc tests are used as a follow up to the significance tests (like t-test or ANOVA). Pairwise t-tests can be used to compare two conditions when both normality of distributions and homogeneity of variances are met.";
 desc["method"]["pwT"] = "Post-hoc tests are used as a follow up to the significance tests (like t-test or ANOVA). Pairwise t-tests can be used to compare two conditions when homogeneity of variances is met. It doens't require the distributions to be normal.";
@@ -45,36 +45,47 @@ desc["method"]["mulR"] = "Multiple regression is an extension of linear regressi
 
 desc["effect-size"] = new Object();
 
-desc["effect-size"]["d"] = "Cohen's d is an effect size. It indicates the difference between the means of two distributions in terms of the pooled standard deviation. 0.2, 0.5, and 0.8 are considered as general guidelines for small, medium, and large effect sizes respectively.";
-desc["effect-size"]["r"] = "Pearson's correlation coefficient r is the measure of correlation between two distributions. -1 indicates a perfect negative correlation, +1 indicates a perfect positive correlation, and 0 indicates no relation.";
-desc["effect-size"]["eS"] = "Eta squared value describes the amount of variance accounted for in the sample.";
-desc["effect-size"]["𝜏"] = "It is a measure of rank correlation, i.e., the similarity of the orderings of the data when ranked by each of the quantities. It is named after Maurice Kendall, who developed it in 1938.";
-desc["effect-size"]["rS"] = "R squared values describes the amount of variance accounted for in the sample. In case of simple linear regression, it is the square of the pearson's correlation coefficient r.";
+desc["effect-size"]["d"] = "An effect-size indicates the magnitude of the effect of the independent variable(s) on the dependent variable. Cohen's d is used when there are two conditions. It quantifies the difference between the means of the two distributions as a factor of the pooled standard deviation (for example, d = 0.5 indicates that the two distributions are 0.5 x standard deviations apart).";
+desc["effect-size"]["r"] = "Pearson's correlation coefficient is used to find correlation (the direction of change in one variable as the other variable changes) between two ratio variables. It has a range of -1 to +1. -1 implies that one variable decreases as the other increases (negative correlation) and +1 implies one variable increases as the other increases (positive correlation). 0 implies there is no dependence between the two variables.";
+desc["effect-size"]["eS"] = "Eta squared value indicates what amount of variance (change) in the dependent variable is caused due to the independent variable(s). It has a range of 0 to 1.";
+desc["effect-size"]["𝜏"] = "It is a measure of correlation. A value of +1 indicates a positive correlation and a value of -1 indicates a negative correlation.";
+desc["effect-size"]["rS"] = "R-squared values describes the amount of variance in the dependent variable is caused due to the independent variable(s). It has a range of 0 to 1. For a simple linear regression, it is the square of the pearson's correlation coefficient \'r\'";
 
 desc["assumptions"] = new Object(); 
 
-desc["assumptions"]["normality"] = "This assumption checks if the distributions of the group are normal distributions (i.e., if they follow a Gaussian distribution characteristic of a bell-curve).";
-desc["assumptions"]["homogeneity"] = "Homogeneity of variances means that the variances of the distributions are roughly the same.";
+desc["assumptions"]["normality"] = "This assumption is met when the distributions that are compared are normally distributed. A normal-distribution or Gaussian distribution can be identified by a characteristic bell-shaped curve.";
+desc["assumptions"]["homogeneity"] = "This assumption is met when the distributions have almost similar variances (the spread of the data).";
 
-desc["variancePlot"] = "This plot compares the variances of the distributions. If the bars are green in color, the distributions have approximately same variance (homogeneity of variance). If the bars are in red, the distributions have different variances (heterogeneity of variances).";
-desc["normalityPlot"] = "The shape of the curve indicates the type of distribution. Normal/Gaussian distributions are characterised by a bell-curve. Distributions that are approximately normal are plotted in green, whereas distributions that are not normal are plotted in red.";
+desc["variancePlot"] = "A plot of the variances of the distributions in the boxplot. If the bars are green in color, the distributions have approximately same variance (homogeneity of variance). If the bars are in red, the distributions have different variances (heterogeneity of variances).";
+desc["normalityPlot"] = "The shape of the curve indicates the type of distribution. Normal/Gaussian distributions are characterised by a bell-curve. Distributions that are approximately normal are plotted in green, whereas distributions that are not normal are plotted in red. Sometimes, it is hard to interpret whether this assumption has been satisfied or not from the shape of the distribution.";
 
-desc["compareMean"] = "Click on this to selects means of the distributions you want to compare.";
+desc["compareMean"] = "Click on this to select the means of the distributions you want to compare.";
 desc["compareNow"] = "Click on this to compare the means! Make sure at least 2 means are selected!";
-desc["tukeyHSD"] = "Click on this to perform a Tukey's HSD test. This pairwise-compares each level of the plot against every other level in the boxplot.";
-desc["pairwisePostHoc"] = "Click on this to select means of the two levels you want to perform a pairwise post-hoc test on.";
-desc["regression"] = "Click on this to construct a regression model (predicting a variable from other variable(s)).";
+desc["tukeyHSD"] = "Click on this to perform a Tukey's HSD test. Tukey's HSD test is a post-hoc test that is used after a significance test is applied. It is used to find which particular pair of distributions are significantly different from each other. This pairwise-compares each level of the plot against every other level in the boxplot.";
+desc["pairwisePostHoc"] = "Click on this to select means of the two levels you want to perform a pairwise post-hoc test on.  It is used to find which particular pair of distributions are significantly different from each other. ";
+desc["regression"] = "Click on this to construct a regression model (which can be used to predict the outcome variable from the causal/explanatory variable(s)).";
 
 desc["variables"] = new Object();
 
 desc["variables"]["participantID"] = "The ID of the participant";
-desc["variables"]["keyboardLayout"] = "The keyboard layout used by the participant (\"QWERTY\", \"DVORAK\", \"i10\")";
-desc["variables"]["gender"] = "The gender of the participant";
-desc["variables"]["typingSpeed"] = "The typing speed of the participant in Words Per Minute (WPM).";
-desc["variables"]["errors"] = "The number of errors made by the participant in a minute";
-desc["variables"]["userSatisfaction"] = "The rating given by the participant after using the keyboard layout.";
+desc["variables"]["userSatisfaction"] = "The rating given by the participant after the experiment.";
+desc["variables"]["satisfaction"] = "The rating given by the participant after the experiment.";
+desc["variables"]["userRating"] = "The rating given by the participant after the experiment.";
+desc["variables"]["gender"] = "The gender of the participant (\"male\", \"female\").";
 
-desc["variables"]["phoneOS"] = "The OS used by the participant (iOS, android, or windows)";
-desc["variables"]["happScore"] = "The happiness score of the participant (out of 100)";
-desc["variables"]["stressScore"] = "The stress score of the participant (out of 100)";
-desc["variables"]["satisfaction"] = "The rating given by the participant after using the keyboard layout.";
+desc["variables"]["keyboardLayout"] = "The keyboard layout used by the participant (\"QWERTY\", \"DVORAK\", \"i10\")";
+desc["variables"]["typingSpeed"] = "The typing speed of the participant in Words Per Minute (WPM).";
+desc["variables"]["errors"] = "The number of errors made by the participant in a minute. ";
+
+desc["variables"]["phoneOS"] = "The OS used by the participant (\"iOS\", \"android\", or \"windows\")";
+desc["variables"]["happScore"] = "The happiness score of the participant (out of 100). Higher the score, more happy the user is.";
+desc["variables"]["stressScore"] = "The stress score of the participant (out of 100). Higher the score, more stressed the user is.";
+
+desc["variables"]["foodEaten"] = "The food eaten by the participant before taking the test (\"yoghurt\", \"snickers\", and \"sandwich\")";
+desc["variables"]["score_V"] = "The verbal (or language) score of the participant in the test (out of 100).";
+desc["variables"]["score_Q"] = "The quantitative (or math) score of the participant in the test (out of 100).";
+
+desc["variables"]["condition"] = "The weight-loss condition of the participant (\"noBreakfast\", \"noLunch\", \"noDinner\", and \"control\")";
+desc["variables"]["exercise"] = "The level of exercise of the participant during the weight-loss period (\"none\", \"low\", and \"high\")";
+desc["variables"]["weightLost"] = "The amount of weight lost by the participant in the weight-loss period (positive value indicates weight was lost and negative value indicates weight was gained)";
+desc["variables"]["BMI"] = "The BMI (Body Mass Index) of the participant after the weight-loss period.";
